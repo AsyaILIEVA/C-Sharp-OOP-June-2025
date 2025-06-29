992d9475a0de439a430b313d39e3bf16bec02a25
ASYA <asyailieva@yahoo.com>
2025-06-29 14:06:29 +0100

    Add solutions to Inheritance - Exercise
    Add solutions to Inheritance - Exercise

---
+++ 1. Inheritance/1.2 Inheritance - Exercise/04. NeedForSpeed/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            FamilyCar car = new(150, 100);
+            car.Drive(10);
+            Console.WriteLine(car.Fuel);
+        }
+    }
+}
