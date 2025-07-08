6daeedf827c16142aedec8902263910f5bd24571
ASYA <asyailieva@yahoo.com>
2025-07-08 22:45:52 +0100

    Added solution to Interfaces and Abstraction
    Added solution to Interfaces and Abstraction

---
+++ 3. Interfaces and Abstraction/3.1 Interfaces and Abstraction - Lab/01. Shapes/Circle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public class Circle : IDrawable
+    {
+        private int radius;
+
+        public Circle(int radius)
+        {
+            this.radius = radius;
+        }
+
+        public int Radius
+        {
+            get { return this.radius; }
+        }
+        public void Draw()
+        {
+            double rIn = Radius - 0.4;
+            double rOut = Radius + 0.4;
+
+            for (double y = Radius; y >= -Radius; --y)
+            {
+                for (double x = -Radius; x < rOut; x += 0.5)
+                {
+                    double value = (x * x) + (y * y);
+
+                    if (value >= rIn * rIn && value <= rOut * rOut)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 3. Interfaces and Abstraction/3.1 Interfaces and Abstraction - Lab/01. Shapes/Rectangle.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        private int width;
+        private int height;
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public int Width 
+        {
+            get { return this.width; } 
+        }
+        public int Height 
+        {
+            get { return this.height; }
+        }
+
+        public void Draw()
+        {
+            DrawLine(Width, '*', '*');
+
+            for (int i = 1; i < Height; ++i)
+            {
+                DrawLine(Width, '*', ' ');
+            }
+
+            DrawLine(Width, '*', '*');
+        }
+
+        private void DrawLine(int width, char end, char mid)
+        {
+            Console.Write(end);
+
+            for (int i = 1; i < width - 1; ++i)
+            {
+                Console.Write(mid);
+            }
+
+            Console.WriteLine(end);
+        }
+    }
+}
