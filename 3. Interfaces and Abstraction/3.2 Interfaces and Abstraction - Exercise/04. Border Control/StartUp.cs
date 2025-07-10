eef18274a4632eccbc0fb4e0a72dc9d069aa95be
ASYA <asyailieva@yahoo.com>
2025-07-10 22:13:40 +0100

    Add solution to Interfaces and Abstraction Exercise
    Add solution to Interfaces and Abstraction Exercise

---
+++ 3. Interfaces and Abstraction/3.2 Interfaces and Abstraction - Exercise/04. Border Control/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using BorderControl.Models;
+using BorderControl.Models.Interfaces;
+using System.Linq;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        List<IIdentifiable> society = new();
+
+        string input;
+        while ((input = Console.ReadLine()) != "End")
+        {  
+            string[] tokens = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            IIdentifiable identifiable;
+
+            if (tokens.Length == 3)
+            {
+                identifiable = new Citizens(tokens[0], int.Parse(tokens[1]), tokens[2]);                
+            }
+            else
+            {
+                identifiable = new Robots(tokens[0], tokens[1]);                
+            }
+
+            society.Add(identifiable);
+        }
+
+        string invalidSuffix = Console.ReadLine();
+        
+        foreach (IIdentifiable member in society)
+        {
+            if (member.Id.EndsWith(invalidSuffix))
+            {
+                Console.WriteLine(member.Id);
+            }
+        }
+    }
+}
---
+++ 3. Interfaces and Abstraction/3.2 Interfaces and Abstraction - Exercise/05. Birthday Celebrations/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using BorderControl.Models;
+using BorderControl.Models.Interfaces;
+using System.Linq;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        List<IBirthable> society = new();
+
+        string input;
+        while ((input = Console.ReadLine()) != "End")
+        {
+            string[] tokens = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string type = tokens[0];
+            IBirthable birthable = default;
+
+            switch (type)
+            {
+                case "Citizen":
+                    birthable = new Citizens(tokens[1], int.Parse(tokens[2]), tokens[3], tokens[4]);
+                    society.Add(birthable);
+                    break;
+                case "Pet":
+                    birthable = new Pet(tokens[1], tokens[2]);
+                    society.Add(birthable);
+                    break;
+                default:
+                    break;
+            }           
+        }    
+
+    string year = Console.ReadLine();
+
+        foreach (IBirthable member in society)
+        {
+            if (member.Birthdate.EndsWith(year))
+            {
+                Console.WriteLine(member.Birthdate);
+            }
+        }
+    }
+}
