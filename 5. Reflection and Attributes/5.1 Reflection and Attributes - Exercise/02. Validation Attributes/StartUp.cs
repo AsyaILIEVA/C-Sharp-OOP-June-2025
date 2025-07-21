64c9495acc4a1d2c645e6d44d18b522297781f98
ASYA <asyailieva@yahoo.com>
2025-07-21 16:44:10 +0100

    Add new solution to Reflection and Attributes - Exercise
    Add new solution to Reflection and Attributes - Exercise

---
+++ 5. Reflection and Attributes/5.1 Reflection and Attributes - Exercise/02. Validation Attributes/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+//using System.ComponentModel.DataAnnotations;
+using ValidationAttributes.Models;
+using ValidationAttributes.Utils;
+
+namespace ValidationAttributes
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            //var person = new Person
+            // (
+            //     null,
+            //     -1
+            // );
+
+            //bool isValidEntity = Validator.IsValid(person);
+
+            //Console.WriteLine(isValidEntity);
+
+            Person person = new(null, -1);
+
+            Console.WriteLine(Validator.IsValid(person));
+        }
+    }
+}
